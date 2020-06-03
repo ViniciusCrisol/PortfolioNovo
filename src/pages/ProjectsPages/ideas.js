41bcaccbb2138e5f783166db935b40a0f99f495c
Vinicius Crisol <61323173+ViniciusCrisol@users.noreply.github.com>
2020-06-03 19:26:21 -0300

    Commit

--- src/pages/ProjectsPages/ideas.js
+++ src/pages/ProjectsPages/ideas.js
@@ -12,7 +12,7 @@ export default function productHunt() {
   return (
     <>
       <Pages
-        projectName="Drinks"
+        projectName="Ideas"
         projectDescription="Projeto desenvolvido com o obejtivo de estudar ReactJs e Node. No desenvolvimento do back end usei Node com o banco MongoDB, foquei na parte de autenticação com JWT e o resultado foi maior que o esperado. Já no front foquei no Redux. O projeto como um todo foi muito bom de se desenvolver, consequentemente o resultado não poderia ser diferente de um projeto completo 100% funcional."
         disableDesc
         repositoryLink="https://github.com/ViniciusCrisol/Ideas"
